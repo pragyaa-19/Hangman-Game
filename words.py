eec163d14439f22ebfc5d5baaf45ad1f0ef42378
Pragya <98525300+pragyaa-19@users.noreply.github.com>
2024-10-28 04:05:33 +0530

    Add files via upload

---
+++ words.py
@@ -0,0 +1,179 @@
+word_list = [
+    "cat", 
+    "dog", 
+    "sun",
+    "bat", 
+    "hat", 
+    "bee",
+    "pie",
+    "fan", 
+    "rat",
+    "pen",
+    "fish",
+    "book", 
+    "milk", 
+    "bird", 
+    "snow", 
+    "door", 
+    "star", 
+    "tree",
+    "apple",
+    "tiger",
+    "ocean",
+    "chair",
+    "dream",
+    "piano",
+    "castle",
+    "magic",
+    "honey",
+    "earth",
+    "lemon",
+    "shoes",
+    "jelly",
+    "toast",
+    "green",
+    "queen",
+    "light",
+    "story",
+    "pizza",
+    "beach",
+    "galaxy",
+    "thunder",
+    "cobbler",
+    "journey",
+    "fortune",
+    "mystery",
+    "crystal",
+    "courage",
+    "wisdom",
+    "library",
+    "season",
+    "texture",
+    "sunset",
+    "patience",
+    "blanket",
+    "fortress",
+    "eclipse",
+    "fossil",
+    "laundry",
+    "scribble",
+    "labyrinth",
+    "nebulous",
+    "phoenix",
+    "whimsical",
+    "tranquil",
+    "quizzical",
+    "grotesque",
+    "mirage",
+    "intrigue",
+    "cacophony",
+    "zephyr",
+    "orchestra",
+    "ephemeral",
+    "paradox",
+    "kryptonite",
+    "chivalry",
+    "pharaoh",
+    "silhouette",
+    "labyrinthine",
+    "dichotomy",
+    "apple",
+    "beach",
+    "cake",
+    "duck",
+    "echo",
+    "flame",
+    "glow",
+    "horse",
+    "ice",
+    "jazz",
+    "kite",
+    "lamp",
+    "moon",
+    "nest",
+    "orange",
+    "pearl",
+    "queen",
+    "rose",
+    "stone",
+    "tulip",
+    "umbrella",
+    "violin",
+    "whale",
+    "xray",
+    "yarn",
+    "zebra"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
